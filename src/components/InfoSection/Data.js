06a4c90d916910baf8f3be6982fcc5fb4a50c052
JC <sanjothebay@gmail.com>
2021-03-11 23:27:55 -0600

    Images fixed

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,3 +1,6 @@
+import JC from "../../images/JC.JPG";
+import DeskTop from "../../images/desk.JPG";
+
 export const homeObjOne = {
   id: "about",
   lightBg: false,
@@ -9,7 +12,7 @@ export const homeObjOne = {
     "Throughout my career, I have contributed to positive business results through effective organization, prioritization and follow-through of key organizational projects. My strengths and qualifications are an ideal match for the Service Desk Technician requirements and will bring immediate value to the Company. In my former Service Desk Technician role, I exercised a calculated and methodical approach to problem-solving. While I am independently motivated, I appreciate collective efforts and collaborate productively within group settings. Moreover, I am competent in writing and data analysis with proficiency in process development. This opportunity is especially exciting as my professional goals align with the IT Industry mission and values. Further, my customer service, critical thinking and problem-solving abilities will serve to support your continued organizational efforts.To illustrate the scope of my career history and professional competencies, please take a moment to review my enclosed resume. I am grateful for your evaluation of my credentials and subsequent response.",
   buttonLabel: "¡¡Contact me¡¡",
   imgStart: false,
-  img: ("../../../public/images/JC"),
+  img: JC,
   alt: "Picture",
   dark: true,
   primary: true,
@@ -26,7 +29,7 @@ export const homeObjTwo = {
   description: `* Languages in Programming: HTML, CSS, JavaScript, JQuery, Python.  * Databases: MySQL, MongoDB, SQLite, NoSQL, SAP Hanna, Oracle, AngularJS. * Tools: GitHub, GitBadh, Heroku, Node.js, VS Code, Bootstrap4, Foundation`,
   buttonLabel: "¡¡portfolio¡¡",
   imgStart: true,
-  img: ("../../images/desk.JPG"),
+  img: DeskTop,
   alt: "portfoliopics",
   dark: false,
   primary: false,
