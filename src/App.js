250eaff5a63bb472ae2c3e7380798f9971667aff
JC <sanjothebay@gmail.com>
2021-04-12 21:05:58 -0500

    gettinh help for githup pages routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
 import ContactmePage from "./pages/contactme";
 
 const App = () => {
   return (
     <Router>
+      <Router basename="/jc_portfolio"></Router>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/contactme" component={ContactmePage} />
