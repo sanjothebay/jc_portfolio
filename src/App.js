39eeaa517f30a4218c8effb56147006f31e6bad7
JC <sanjothebay@gmail.com>
2021-04-13 21:40:11 -0500

    able to download file fixed by removinf the HASHBROWSER

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter  as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
 import ContactmePage from "./pages/contactme";
 
 const App = () => {
   return (
-    <Router basename="/jc_portfolio">
+    <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/contactme" component={ContactmePage} />
+        <Route path="/contactme" component={ContactmePage} />
       </Switch>
     </Router>
   );
